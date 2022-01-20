c3356950934fd46651120bf304c9d5d06509fd5d
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-01-20 18:53:07 +0100

    Reverse words and celebrates palindromes
    Co-authored-by: Javier Cañete <Javibu5@users.noreply.github.com>

--- src/ohce.js
+++ src/ohce.js
@@ -1,22 +1,57 @@
 export default class {
-  constructor(printer, clock) {
+  constructor(printer, clock, reader) {
     this.printer = printer;
     this.clock = clock;
+    this.reader = reader;
   }
 
-  run(input) {
+  start(userName) {
+    this.greets(userName);
+
+    while (true) {
+      const word = this.reader.get();
+
+      if (word === 'Stop!') {
+        break;
+      }
+
+      this.printer(reverse(word));
+
+      if (isPalindrome(word)) {
+        this.printer('¡Bonita palabra!');
+      }
+    }
+
+    this.printer(`Adiós ${userName}`);
+  }
+
+  greets(userName) {
     const currentHour = this.clock.hour();
-    const isMorningTime = currentHour >= 6 && currentHour <= 12;
-    const isAfternoonTime = currentHour >= 13 && currentHour <= 20;
+    const greeting = getGreeting(currentHour, userName);
 
-    if (isMorningTime) {
-      return this.printer(`¡Buenos días ${input}!`);
-    }
-
-    if (isAfternoonTime) {
-      return this.printer(`¡Buenas tardes ${input}!`);
-    }
-
-    this.printer(`¡Buenas noches ${input}!`);
+    this.printer(greeting);
   }
+}
+
+function isPalindrome(word) {
+  return word === reverse(word);
+}
+
+function getGreeting(currentHour, userName) {
+  const isMorningTime = currentHour >= 6 && currentHour <= 12;
+  const isAfternoonTime = currentHour >= 13 && currentHour <= 20;
+
+  if (isMorningTime) {
+    return `¡Buenos días ${userName}!`;
+  }
+
+  if (isAfternoonTime) {
+    return `¡Buenas tardes ${userName}!`;
+  }
+
+  return `¡Buenas noches ${userName}!`;
+}
+
+function reverse(word) {
+  return word.split('').reverse().join('');
 }
--- src/ohce.test.js
+++ src/ohce.test.js
@@ -1,39 +1,104 @@
 import Ohce from './ohce';
 
-describe('Greets you', () => {
-  const input = 'Pedro';
+describe('Ohce', () => {
+  const userName = 'Pedro';
+
   let printer;
   let clock = {};
+  let reader = {};
 
   beforeEach(() => {
     printer = jest.fn();
     clock.hour = jest.fn();
+    reader.get = jest.fn();
   });
 
-  test.each([10, 6, 12])('in the morning', (hour) => {
-    clock.hour.mockReturnValue(hour);
+  describe('Greets you', () => {
+    test.each([10, 6, 12])('in the morning at %p', (hour) => {
+      clock.hour.mockReturnValue(hour);
+      reader.get.mockReturnValueOnce('Stop!');
 
-    const ohce = new Ohce(printer, clock);
-    ohce.run(input);
+      const ohce = new Ohce(printer, clock, reader);
+      ohce.start(userName);
 
-    expect(printer).toHaveBeenCalledWith(`¡Buenos días Pedro!`);
+      expect(printer).toHaveBeenCalledWith(`¡Buenos días Pedro!`);
+    });
+
+    test.each([16, 13, 20])('in the afternoon at %p', (hour) => {
+      clock.hour.mockReturnValue(hour);
+      reader.get.mockReturnValueOnce('Stop!');
+
+      const ohce = new Ohce(printer, clock, reader);
+      ohce.start(userName);
+
+      expect(printer).toHaveBeenCalledWith(`¡Buenas tardes Pedro!`);
+    });
+
+    test.each([22, 21, 23, 0, 5])('in the night at %p', (hour) => {
+      clock.hour.mockReturnValue(hour);
+      reader.get.mockReturnValueOnce('Stop!');
+
+      const ohce = new Ohce(printer, clock, reader);
+      ohce.start(userName);
+
+      expect(printer).toHaveBeenCalledWith(`¡Buenas noches Pedro!`);
+    });
+
+    test.each([10, 19, 23])('with your name at %p', (hour) => {
+      clock.hour.mockReturnValue(hour);
+      reader.get.mockReturnValueOnce('Stop!');
+
+      const ohce = new Ohce(printer, clock, reader);
+      ohce.start('Javi');
+
+      expect(printer).toHaveBeenCalledWith(expect.stringMatching(/.+Javi!/));
+    });
   });
 
-  test.each([16, 13, 20])('in the afternoon', (hour) => {
-    clock.hour.mockReturnValue(hour);
+  describe('Says goodbye', () => {
+    test('with your name', () => {
+      clock.hour.mockReturnValue(10);
+      reader.get.mockReturnValueOnce('Stop!');
 
-    const ohce = new Ohce(printer, clock);
-    ohce.run(input);
+      const ohce = new Ohce(printer, clock, reader);
+      ohce.start('Javi');
 
-    expect(printer).toHaveBeenCalledWith(`¡Buenas tardes Pedro!`);
+      expect(printer).toHaveBeenCalledWith('Adiós Javi');
+    });
   });
 
-  test.each([22, 21, 23, 0, 5])('in the night', (hour) => {
-    clock.hour.mockReturnValue(hour);
+  describe('Listen to words', () => {
+    test('reverse words', () => {
+      reader.get
+        .mockReturnValueOnce('word')
+        .mockReturnValueOnce('javi')
+        .mockReturnValueOnce('Stop!');
+      clock.hour.mockReturnValue(10);
+      const ohce = new Ohce(printer, clock, reader);
 
-    const ohce = new Ohce(printer, clock);
-    ohce.run(input);
+      ohce.start(userName);
 
-    expect(printer).toHaveBeenCalledWith(`¡Buenas noches Pedro!`);
+      expect(printer.mock.calls).toEqual([
+        [`¡Buenos días ${userName}!`],
+        ['drow'],
+        ['ivaj'],
+        [`Adiós ${userName}`],
+      ]);
+    });
+
+    test('detects palindromes', () => {
+      reader.get.mockReturnValueOnce('ana').mockReturnValueOnce('Stop!');
+      clock.hour.mockReturnValue(10);
+      const ohce = new Ohce(printer, clock, reader);
+
+      ohce.start(userName);
+
+      expect(printer.mock.calls).toEqual([
+        [`¡Buenos días ${userName}!`],
+        ['ana'],
+        ['¡Bonita palabra!'],
+        [`Adiós ${userName}`],
+      ]);
+    });
   });
 });
