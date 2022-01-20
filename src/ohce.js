fb98bd00f0484d771c7146db0886946aabfcbe7e
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-01-20 18:52:28 +0100

    Greets with the correct time
    Co-authored-by: Javier Cañete <Javibu5@users.noreply.github.com>

--- src/ohce.js
+++ src/ohce.js
@@ -1,3 +1,22 @@
-export default () => {
-  return false;
+export default class {
+  constructor(printer, clock) {
+    this.printer = printer;
+    this.clock = clock;
+  }
+
+  run(input) {
+    const currentHour = this.clock.hour();
+    const isMorningTime = currentHour >= 6 && currentHour <= 12;
+    const isAfternoonTime = currentHour >= 13 && currentHour <= 20;
+
+    if (isMorningTime) {
+      return this.printer(`¡Buenos días ${input}!`);
+    }
+
+    if (isAfternoonTime) {
+      return this.printer(`¡Buenas tardes ${input}!`);
+    }
+
+    this.printer(`¡Buenas noches ${input}!`);
+  }
 }
--- src/ohce.test.js
+++ src/ohce.test.js
@@ -1,5 +1,39 @@
-import ohce from "./ohce";
+import Ohce from './ohce';
 
-test("canary test", () => {
- expect(false).toEqual(true);
+describe('Greets you', () => {
+  const input = 'Pedro';
+  let printer;
+  let clock = {};
+
+  beforeEach(() => {
+    printer = jest.fn();
+    clock.hour = jest.fn();
+  });
+
+  test.each([10, 6, 12])('in the morning', (hour) => {
+    clock.hour.mockReturnValue(hour);
+
+    const ohce = new Ohce(printer, clock);
+    ohce.run(input);
+
+    expect(printer).toHaveBeenCalledWith(`¡Buenos días Pedro!`);
+  });
+
+  test.each([16, 13, 20])('in the afternoon', (hour) => {
+    clock.hour.mockReturnValue(hour);
+
+    const ohce = new Ohce(printer, clock);
+    ohce.run(input);
+
+    expect(printer).toHaveBeenCalledWith(`¡Buenas tardes Pedro!`);
+  });
+
+  test.each([22, 21, 23, 0, 5])('in the night', (hour) => {
+    clock.hour.mockReturnValue(hour);
+
+    const ohce = new Ohce(printer, clock);
+    ohce.run(input);
+
+    expect(printer).toHaveBeenCalledWith(`¡Buenas noches Pedro!`);
+  });
 });
